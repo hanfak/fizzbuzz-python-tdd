715c1cfb7e4021ea9e86fd6542d9ff143c76bfd2
Hanif Fakira <fakira.work@gmail.com>
2016-06-15 14:19:37 +0100

    separting if logic to private functions

--- app/game.py
+++ app/game.py
@@ -1,12 +1,21 @@
 class FizzBuzz(object):
 	def count(self,number):
-		if number % 15 == 0:
+		if self.__divisible_by_15(number):
 			return "FizzBuzz"
 
-		if number % 3 == 0:
+		if self.__divisible_by_3(number):
 			return "Fizz"
 
-		if number % 5 == 0:
+		if self.__divisible_by_5(number):
 			return "Buzz"
 
 		return str(number)
+
+	def __divisible_by_3(self,number):
+		return number % 3 == 0
+
+	def __divisible_by_5(self,number):
+		return number % 5 == 0
+
+	def __divisible_by_15(self,number):
+		return number % 15 == 0
