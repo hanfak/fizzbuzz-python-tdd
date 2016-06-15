02577d3e5df007ff7bf73f4a867101764bb5f51f
Hanif Fakira <fakira.work@gmail.com>
2016-06-15 14:28:51 +0100

    refactor: move private functions to one

--- app/game.py
+++ app/game.py
@@ -1,21 +1,15 @@
 class FizzBuzz(object):
 	def count(self,number):
-		if self.__divisible_by_15(number):
+		if self.__divisible_by(number, 15):
 			return "FizzBuzz"
 
-		if self.__divisible_by_3(number):
+		if self.__divisible_by(number, 3):
 			return "Fizz"
 
-		if self.__divisible_by_5(number):
+		if self.__divisible_by(number, 5):
 			return "Buzz"
 
 		return str(number)
 
-	def __divisible_by_3(self,number):
-		return number % 3 == 0
-
-	def __divisible_by_5(self,number):
-		return number % 5 == 0
-
-	def __divisible_by_15(self,number):
-		return number % 15 == 0
+	def __divisible_by(self, number, a):
+		return number % a == 0
